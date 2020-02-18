f1e37759f2d5edbe92e3559d07e08f8f9a5ef52b
Gabriel <gabriel.justo@acad.pucrs.br>
2020-02-18 18:40:53 -0300

    working

---
+++ BinToDec/Home.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="BinToDec.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Binary to decimal converter<br />
+            <br />
+            Decimal:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+            <asp:TextBox ID="TextBox_Decimal" runat="server"></asp:TextBox>
+&nbsp;
+            <asp:Button ID="Button1" runat="server" Text="Convert" />
+            <br />
+            Binary:<br />
+            <asp:Label ID="Label_Binary" runat="server" Text="Label"></asp:Label>
+            <br />
+            <br />
+            <br />
+            <br />
+            Binary:<br />
+            <asp:TextBox ID="TextBox_Binary" runat="server"></asp:TextBox>
+            <asp:Button ID="Button_BinToDec" runat="server" OnClick="Button_BinToDec_Click" Text="Convert" />
+            <br />
+            <br />
+            Decimal:<br />
+            <asp:Label ID="Label_Decimal" runat="server" Text="Label"></asp:Label>
+            <br />
+        </div>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ BinToDec/Home.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BinToDec
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void Button_Convert_Click(object sender, EventArgs e)
+        {
+            int dec;
+            int.TryParse(TextBox_Decimal.Text, out dec);
+            Label_Binary.Text = DecToBin(dec);
+        }
+
+        protected void Button_BinToDec_Click(object sender, EventArgs e)
+        {
+            if(BinToDec(TextBox_Binary.Text) == -1)
+                Label_Decimal.Text = "You must write only \"1\" or \"0\"";
+            else
+                Label_Decimal.Text = BinToDec(TextBox_Binary.Text).ToString();
+        }
+
+
+
+        public static int BinToDec (string bin)
+        {
+            int j = bin.Length - 1;
+            System.Diagnostics.Debug.WriteLine("binary length: " + bin.Length);
+            int dec = 0;
+
+            for(int i = 0; i < bin.Length; i++)
+            {
+                System.Diagnostics.Debug.WriteLine("[" + i + "]: " + bin.ElementAt(i));
+                if (bin.ElementAt(i).Equals('1'))
+                {
+                    dec += (int)Math.Pow(2, j);
+                    System.Diagnostics.Debug.WriteLine("2^" + j);
+                }else if (!bin.ElementAt(i).Equals('0'))
+                {
+                    return -1;
+                }
+                j--;
+            }
+            return dec;
+        }
+
+        public static string DecToBin (int dec)
+        {
+            string bin = "";
+            int aux;
+
+            while (dec != 0)
+            {
+                aux = dec % 2;
+                bin = aux.ToString() + bin;
+                dec /= 2;
+            }
+
+            return bin;
+        }
+
+
+    }
+}
